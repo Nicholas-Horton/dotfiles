6d6bcfc2d974d483c37a285521a6d27dcc55d838
Nicholas-Horton <nicholas.oryan.horton@gmail.com>
2017-06-14 15:54:44 -0400

    [FZF] Adds more reasonable default ALT-C functionality

--- home/.fzf.zsh
+++ home/.fzf.zsh
@@ -17,5 +17,6 @@ source "/home/nhorton/.fzf/shell/key-bindings.zsh"
 if command -v rg >/dev/null 2>&1; then
   export FZF_DEFAULT_COMMAND='rg --hidden --ignore-file .git -g "" --files'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-  export FZF_DEFAULT_OPTS='--height 40% --reverse --bind ctrl-u:unix-line-discard+top
+  export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200"
+  export FZF_DEFAULT_OPTS='--height 40% --reverse --bind ctrl-u:unix-line-discard+top'
 fi
