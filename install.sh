fae1da83133628e04987f707264a772548b6c458
Nicholas-Horton <nicholas.oryan.horton@gmail.com>
2017-10-13 09:36:38 -0400

    [XPROFILE] Adds xcape and xbindkeys to xprofile
    This show only start xcape and xbindkeys once. While this doesn't start
    them in cases where x isn't running, I think this is good enough of a
    compromise for now.
    
    Could make this better by finding a configuration that will start with a
    given tty. Not really sure if that is even a thing, or something you'd
    even want to do in the first place.
    
    Works for now!

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ ln -si $CURRENT_PATH/home/.vimrc ~/.vimrc
 ln -si $CURRENT_PATH/home/.gitconfig ~/.gitconfig
 ln -si $CURRENT_PATH/home/.tmux.conf ~/.tmux.conf
 ln -si $CURRENT_PATH/home/.xbindkeysrc ~/.xbindkeysrc
+ln -si $CURRENT_PATH/home/.xprofile ~/.xprofile
 
 # Weechat
 ln -si $CURRENT_PATH/home/.weechat ~/.weechat
